d0b2f363d1e43dcacb5218aaaf4c5cab446ebd0f
jean11cj <jeancacaji@gmail.com>
2025-11-17 16:28:27 -0500

    Interfaces

---
+++ Documents/NetBeansProjects/SistemaNomina/src/com/nomina/interfaces/Beneficiable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.nomina.interfaces;
+
+/**
+ *
+ * @author Jean Carlos
+ */
+public interface Beneficiable {
+    double calcularBeneficios();
+}
+
