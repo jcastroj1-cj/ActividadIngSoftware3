aea80b6e2c0b6ea73ed692b1a6faa26669cf1f57
jcastroj1-cj <jcastroj1@unicartagena.edu.co>
2025-11-19 21:31:21 -0500

    Delete Downloads directory

--- Downloads/gastos_springboot_hexagonal/src/main/java/com/jcaa/gastos/adapter/databases/sql/GastoJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jcaa.gastos.adapter.databases.sql;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GastoJpaRepository extends JpaRepository<GastoEntity, Long> {
-}
